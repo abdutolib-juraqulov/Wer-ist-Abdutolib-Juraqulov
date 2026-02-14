19247912fd9c2a00d029d127ce8b09e890c259cb
Abdutolib Juraqulov <juraqulovabdutolib1@gmail.com>
2026-02-14 11:51:18 +0100

    Über mich in 4 Sprachen

---
+++ Mein Portfolio/script.js
@@ -0,0 +1,39 @@
+function translateToEnglish() {
+    headline.innerHTML = 'Hello, i am  Abdutolib Juraqulov';
+    text1.innerHTML = 'I am applying for a training position as an IT specialist in application development because I have a strong interest in IT and software development and see my professional future in this field.';
+    text2.innerHTML = 'I am currently in the third year of training as a media technologist in print processing. At the same time, I have been intensively involved in programming over the past two years. Initially, I taught myself the basics independently and then further developed my skills. I am particularly confident in working with HTML, CSS, JavaScript, and Python.';
+    text3.innerHTML = 'Working with computers and solving technical problems are not just learning content for me—they are also a personal hobby. I have implemented my own small projects as well as this website and published them on GitHub to showcase my practical skills and continuously improve them.';
+    text4.innerHTML = 'I work in a structured manner, am motivated, and learn new content quickly. In the training program for IT specialists in application development, I want to deepen my existing knowledge, learn professional development processes, and develop further as a software developer in the long term.';
+    text5.innerHTML = 'I would be very pleased to receive an invitation for a personal interview.';
+    copyright.innerHTML = '© 2026 Abdutolib Juraqulov. All rights reserved.';
+}
+
+function translateToGerman(){
+    headline.innerHTML = 'Hallo , ich bin Abdutolib Juraqulov';
+    text1.innerHTML = 'Ich bewerbe mich um einen Ausbildungsplatz zum Fachinformatiker für Anwendungsentwicklung, da ich großes Interesse an IT und Softwareentwicklung habe und meine berufliche Zukunft in diesem Bereich sehe.';
+    text2.innerHTML = 'Derzeit befinde ich mich im dritten Lehrjahr der Ausbildung zum Medientechnologen Druckverarbeitung. Parallel dazu habe ich mich in den letzten zwei Jahren intensiv mit Programmierung beschäftigt. Zu Beginn habe ich mir grundlegende Kenntnisse selbstständig angeeignet und anschließend meine Fähigkeiten weiter ausgebaut. Besonders sicher bin ich im Umgang mit HTML, CSS, JavaScript und Python.';
+    text3.innerHTML = 'Die Arbeit mit dem Computer und das Lösen technischer Probleme sind für mich nicht nur Lerninhalte, sondern auch ein persönliches Hobby. Eigene kleine Projekte sowie diese Webseite habe ich selbst umgesetzt und auf GitHub veröffentlicht, um meine praktischen Kenntnisse zu zeigen und kontinuierlich zu verbessern.';
+    text4.innerHTML = 'Ich arbeite strukturiert, bin motiviert und lerne neue Inhalte schnell. In der Ausbildung zum Fachinformatiker für Anwendungsentwicklung möchte ich mein vorhandenes Wissen vertiefen, professionelle Entwicklungsprozesse kennenlernen und mich langfristig als Softwareentwickler weiterentwickeln.';
+    text5.innerHTML = 'Über eine Einladung zu einem persönlichen Gespräch freue ich mich sehr.';
+    copyright.innerHTML = '© 2026 Abdutolib Juraqulov. Alle Rechte vorbehalten.';
+}
+
+function translateToRussian(){
+    headline.innerHTML = 'Здравствуйте, меня зовут Абдутолиб Джуракулов.';
+    text1.innerHTML = 'Я подаю заявку на место для прохождения профессионального обучения по специальности «Специалист по информационным технологиям (разработка приложений)», так как я проявляю большой интерес к IT и разработке программного обеспечения и вижу своё профессиональное будущее в этой сфере.';
+    text2.innerHTML = 'В настоящее время я нахожусь на третьем году обучения по профессии «Медиатехнолог печатной обработки». Параллельно с этим в течение последних двух лет я интенсивно занимался программированием. Сначала я самостоятельно освоил базовые знания, а затем последовательно развивал свои навыки. Особенно уверенно я работаю с HTML, CSS, JavaScript и Python.';
+    text3.innerHTML = 'Работа с компьютером и решение технических задач для меня — не только часть обучения, но и личное хобби. Собственные небольшие проекты, а также этот веб-сайт, я реализовал самостоятельно и опубликовал на GitHub, чтобы продемонстрировать свои практические навыки и постоянно их совершенствовать.';
+    text4.innerHTML = 'Я работаю структурированно, мотивирован и быстро осваиваю новые знания. В ходе обучения по специальности «Специалист по информационным технологиям (разработка приложений)» я хотел бы углубить уже имеющиеся знания, познакомиться с профессиональными процессами разработки и в долгосрочной перспективе развиваться как программист.';
+    text5.innerHTML = 'Я буду очень рад приглашению на личное собеседование.';
+    copyright.innerHTML = '© 2026 Абдутолиб Джуракулов. Все права защищены.';
+}
+
+function translateToTajik(){
+    headline.innerHTML = 'Салом, номи ман Абдутолиб Ҷурақулов аст.';
+    text1.innerHTML = 'Ман барои гирифтани ҷои омӯзиши касбӣ ҳамчун мутахассиси технологияҳои иттилоотӣ дар соҳаи таҳияи барномаҳо (Fachinformatiker für Anwendungsentwicklung) муроҷиат менамоям, зеро ба соҳаи IT ва таҳияи нармафзор таваҷҷуҳи зиёд дорам ва ояндаи касбии худро дар ҳамин самт мебинам.';
+    text2.innerHTML = 'Дар айни замон ман дар соли сеюми омӯзиш аз рӯи касби «Медиатехнологи коркарди чоп» қарор дорам. Ҳамзамон, дар давоми ду соли охир ман ба омӯзиши барномасозӣ машғулияти ҷиддӣ доштам. Дар оғоз ман донишҳои ибтидоиро мустақилона аз худ намудам ва баъдан малакаҳои худро пайваста такмил додам. Ман махсусан дар кор бо HTML, CSS, JavaScript ва Python худро боэътимод ҳис мекунам.';
+    text3.innerHTML = 'Кор бо компютер ва ҳал кардани масъалаҳои техникӣ барои ман на танҳо қисми омӯзиш, балки ҳамчунин як шавқи шахсӣ мебошад. Ман лоиҳаҳои хурди шахсии худро ва инчунин ин вебсайтро мустақилона амалӣ намуда, онҳоро дар GitHub нашр кардам, то ки донишҳои амалии худро нишон диҳам ва пайваста онҳоро беҳтар созам.';
+    text4.innerHTML = 'Ман ба таври муназзам кор мекунам, ҳавасманд ҳастам ва маълумоти навро зуд аз худ менамоям. Дар ҷараёни омӯзиш ҳамчун мутахассиси таҳияи барномаҳо мехоҳам донишҳои мавҷудаи худро амиқтар намоям, бо равандҳои касбии таҳия шинос шавам ва дар дарозмуддат ҳамчун таҳиягари нармафзор рушд кунам.';
+    text5.innerHTML = 'Ман аз гирифтани даъватнома барои мусоҳибаи шахсӣ хеле хурсанд хоҳам шуд.';
+    copyright.innerHTML = '© 2026 Абдутолиб Ҷурақулов. Ҳамаи ҳуқуқҳо ҳифз шудаанд.';
+}
